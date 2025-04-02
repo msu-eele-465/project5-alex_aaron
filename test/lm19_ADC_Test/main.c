d9e870f9b1017bb22839c7af7311c6483f9b9cb7
Aaron M <aamclean16@gmail.com>
2025-04-02 16:05:32 -0600

    reconfigured project files for test files

--- test/lm19_ADC_Test/main.c
+++ test/lm19_ADC_Test/main.c
@@ -1,82 +1,50 @@
-/* --COPYRIGHT--,BSD
- * Copyright (c) 2018, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * --/COPYRIGHT--*/
-//***************************************************************************************
-//  Blink the LED Demo - Software Toggle P1.0
-//
-//  Description; Toggle P1.0 inside of a software loop using DriverLib.
-//  ACLK = n/a, MCLK = SMCLK = default DCO
-//
-//                MSP4302355
-//             -----------------
-//         /|\|              XIN|-
-//          | |                 |
-//          --|RST          XOUT|-
-//            |                 |
-//            |             P1.0|-->LED
-//
-//  E. Chen
-//  Texas Instruments, Inc
-//  May 2018
-//  Built with Code Composer Studio v8
-//***************************************************************************************
+#include <msp430.h>
 
-#include <driverlib.h>
+unsigned int ADC_Value;
 
-int main(void) {
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
-    volatile uint32_t i;
+    //-- Configure Ports
+    P1DIR |= BIT0;              // Config P1.0 (LED1) as output
+    P6DIR |= BIT6;              // Config P6.6 (LED2) as output
 
-    // Stop watchdog timer
-    WDT_A_hold(WDT_A_BASE);
+    P1SEL1 |= BIT2;             // Configure P1.2 Pin for A2
+    P1SEL0 |= BIT2;
 
-    // Set P1.0 to output direction
-    GPIO_setAsOutputPin(
-        GPIO_PORT_P1,
-        GPIO_PIN0
-        );
+    PM5CTL0 &= ~LOCKLPM5;       // Turn on GPIO
 
-    // Disable the GPIO power-on default high-impedance mode
-    // to activate previously configured port settings
-    PMM_unlockLPM5();
+    //-- Configure ADC
+    ADCCTL0 &= ~ADCSHT;         // Clear ADCSHT from def. of ADCSHT=01
+    ADCCTL0 |= ADCSHT_3;        // Conversion Cycles = 16 (ADCSHT=10)
+    ADCCTL0 |= ADCON;           // ADC = on.
+
+    ADCCTL1 |= ADCSSEL_2;       // ADC Clock Source = SMCLK
+    ADCCTL1 |= ADCSHP;          // Sample signal source = sampling timer
+
+    ADCCTL2 &= ~ADCRES;         // Clear ADCRES from def. of ADCRES=01
+    ADCCTL2 |= ADCRES_2;        // Resolution = 12-bit (ADCRES=10)
+
+    ADCMCTL0 |= ADCINCH_2;      // ADC Input Channel = A2 (P1.2)
 
     while(1)
     {
-        // Toggle P1.0 output
-        GPIO_toggleOutputOnPin(
-            GPIO_PORT_P1,
-            GPIO_PIN0
-            );
+        ADCCTL0 |= ADCENC | ADCSC;    // Enable and Start conversion
+        while((ADCIFG & ADCIFG0) == 0); // wait for conversion complete
+        ADC_Value = ADCMEM0;          // Read ADC result
 
-        // Delay
-        for(i=10000; i>0; i--);
+        if(ADC_Value > 3613)
+        {  
+            P1OUT |= BIT0;            // If (A2 > 3V), LED1=ON (red)
+            P6OUT &= ~BIT6;           // LED2=OFF
+        }
+        else
+        {
+            P1OUT &= ~BIT0;           // If (A2 < 3V), LED1=OFF
+            P6OUT |= BIT6;            // LED2=ON (green)
+        }
     }
+
+    return 0;
 }
